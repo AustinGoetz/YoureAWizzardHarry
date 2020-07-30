17ca893d0e0a3d2a1d04f53e6469ec02e6cc0681
Austin Goetz <a.goetz09@gmail.com>
2020-07-30 13:20:14 -0600

    first commit

---
+++ HogwartsHouses/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HogwartsHouses
+//
+//  Created by Austin Goetz on 7/30/20.
+//  Copyright © 2020 Austin Goetz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
